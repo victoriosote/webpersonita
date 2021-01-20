3977d224cdab9d8c259f976008477885bd306272
victoriosote <a2019112604@estudiantes.upsa.edu.bo>
2021-01-19 21:35:52 -0400

    publicacion en la web del porgama

---
+++ WebApiPersona/WebApiPersona/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+namespace WebApiPersona.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+                        
+        }
+
+        public System.Data.Entity.DbSet<WebApiPersona.Models.Person> People { get; set; }
+    }
+}
